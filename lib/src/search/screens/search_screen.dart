ad19ab8ca24f382758b966bb5cfb6f699b91a3a5
Jorge Sánchez <xurxosf@gmail.com>
2019-09-07 20:38:37 +0200

    Fix layout issues

--- lib/src/search/screens/search_screen.dart
+++ lib/src/search/screens/search_screen.dart
@@ -12,12 +12,14 @@ class SearchScreen extends StatelessWidget {
         children: <Widget>[
           SearchToolbar(),
           Expanded(
-            child: ListView(
-              children: <Widget>[
-                ListTile(
-                    title: Text("Servicios",
-                        style: Theme.of(context).textTheme.headline)),
-                SizedBox(
+              child: ListView(
+            children: <Widget>[
+              ListTile(
+                  title: Text("Servicios",
+                      style: Theme.of(context).textTheme.title)),
+              Padding(
+                padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                child: SizedBox(
                   height: 250.0,
                   child: ListView(
                     physics: ClampingScrollPhysics(),
@@ -26,24 +28,32 @@ class SearchScreen extends StatelessWidget {
                     children: <Widget>[
                       SearchItem(
                         pathImage:
-                        "https://images-eu.ssl-images-amazon.com/images/G/30/Primevideo/Associates/AssocAds_AVD-5164-International-Spain_250x250.jpg",
+                            "https://images-eu.ssl-images-amazon.com/images/G/30/Primevideo/Associates/AssocAds_AVD-5164-International-Spain_250x250.jpg",
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
                       ),
                       SearchItem(
                         pathImage:
-                        "https://images-eu.ssl-images-amazon.com/images/G/30/kindle/ku/associates/PD2019/XCM_Manual1177852_ES_KU_Ad_250x250_1561470311.png",
+                            "https://images-eu.ssl-images-amazon.com/images/G/30/kindle/ku/associates/PD2019/XCM_Manual1177852_ES_KU_Ad_250x250_1561470311.png",
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
                       ),
                       SearchItem(
                         pathImage:
-                        "https://images-eu.ssl-images-amazon.com//images/G/30/digital/music/2019/Associates/020819/ES_020819_EU_MC_Associates_02_300x250._CB454202434_.png",
+                            "https://images-eu.ssl-images-amazon.com//images/G/30/digital/music/2019/Associates/020819/ES_020819_EU_MC_Associates_02_250x250._CB454202434_.png",
                       ),
                     ],
                   ),
                 ),
-
-                ListTile(
-                    title: Text("Ofertas",
-                        style: Theme.of(context).textTheme.headline)),
-                SizedBox(
+              ),
+              ListTile(
+                  title: Text("Ofertas",
+                      style: Theme.of(context).textTheme.title)),
+              Padding(
+                padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                child: SizedBox(
                   height: 250.0,
                   child: ListView(
                     physics: ClampingScrollPhysics(),
@@ -52,23 +62,32 @@ class SearchScreen extends StatelessWidget {
                     children: <Widget>[
                       SearchItem(
                         pathImage:
-                        "https://ae01.alicdn.com/kf/H5a6ac74b1a044657a606837cb18e157en.png",
+                            "https://ae01.alicdn.com/kf/H5a6ac74b1a044657a606837cb18e157en.png",
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
                       ),
                       SearchItem(
                         pathImage:
-                        "https://ae01.alicdn.com/kf/H7866dd7e4b9f477eb27a1f2948c202dbw.png",
+                            "https://ae01.alicdn.com/kf/H7866dd7e4b9f477eb27a1f2948c202dbw.png",
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
                       ),
                       SearchItem(
                         pathImage:
-                        "https://ae01.alicdn.com/kf/H655dde5f67c54346bffda41357ae23d7F.png",
+                            "https://ae01.alicdn.com/kf/H655dde5f67c54346bffda41357ae23d7F.png",
                       ),
                     ],
                   ),
                 ),
-                ListTile(
-                    title: Text("Productos",
-                        style: Theme.of(context).textTheme.headline)),
-                SizedBox(
+              ),
+              ListTile(
+                  title: Text("Productos",
+                      style: Theme.of(context).textTheme.title)),
+              Padding(
+                padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                child: SizedBox(
                   height: 250.0,
                   child: ListView(
                     physics: ClampingScrollPhysics(),
@@ -77,22 +96,28 @@ class SearchScreen extends StatelessWidget {
                     children: <Widget>[
                       SearchItem(
                         pathImage:
-                        "https://images-eu.ssl-images-amazon.com/images/G/30/associates/maitri/banner/ES_title_count_44K_assoc_300x250.gif",
+                            "https://images-eu.ssl-images-amazon.com/images/G/30/associates/maitri/banner/ES_title_count_44K_assoc_300x250.gif",
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
                       ),
                       SearchItem(
                         pathImage:
-                        "https://images-eu.ssl-images-amazon.com/images/G/30/ES-hq/2018/img/Consumer_Electronics/XCM_1103990_Manual_250x250_1103990_es_consumer_electronics_associates_electronic_store_associate_250x250_jpg_Associates_electronic_store.jpg",
+                            "https://images-eu.ssl-images-amazon.com/images/G/30/ES-hq/2018/img/Consumer_Electronics/XCM_1103990_Manual_250x250_1103990_es_consumer_electronics_associates_electronic_store_associate_250x250_jpg_Associates_electronic_store.jpg",
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
                       ),
                       SearchItem(
                         pathImage:
-                        "https://images-eu.ssl-images-amazon.com/images/G/30/associates/22151220_ES_Associates_250x250._CB1198675309_.jpg",
+                            "https://images-eu.ssl-images-amazon.com/images/G/30/associates/22151220_ES_Associates_250x250._CB1198675309_.jpg",
                       ),
                     ],
                   ),
                 ),
-              ],
-            ),
-          )
+              ),
+            ],
+          ))
         ],
       ),
     );
--- lib/src/search/widgets/search_item.dart
+++ lib/src/search/widgets/search_item.dart
@@ -2,33 +2,20 @@ import 'package:flutter/material.dart';
 
 class SearchItem extends StatelessWidget {
   final String pathImage;
-  final double height;
-  final double width;
 
-  SearchItem({
-    Key key,
-    @required this.pathImage,
-    this.height = 250.0,
-    this.width = 250.0
-  });
+  SearchItem({Key key, @required this.pathImage});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      margin: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover, image: NetworkImage(pathImage)),
-          borderRadius: BorderRadius.all(Radius.circular(20.0)),
-          shape: BoxShape.rectangle,
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.black38,
-                blurRadius: 15.0,
-                offset: Offset(0.0, 7.0))
-          ]),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20.0),
+      child: Card(
+        elevation: 4.0,
+        child: Image.network(
+          pathImage,
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
